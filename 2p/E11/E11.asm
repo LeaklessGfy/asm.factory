ac7a8c66cc49701474719c0242237b6e686ea8e6
LeaklessGfy <leakleass@protonmail.com>
2016-10-12 19:49:03 +0200

    Fix E11

--- 2p/E11/E11.asm
+++ 2p/E11/E11.asm
@@ -3,7 +3,7 @@
 ; Objectif : [TP2] Exercice 11 - sort number not in list without .bss
 
 %include "asm_io.inc"
-%define LENGTH 33
+%define LENGTH 32
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;; DATA ;;;;;;;;;;;;;;;;;;;;;;;;;;
 SECTION .data
@@ -21,98 +21,51 @@ get_input:
 	call read_int
 	ret
 
-get_puissance:
-	mov eax, 2
-
-	cmp ebx, 0
-	je p_0
-
-	cmp ebx, 1
-	je p_return
-
-p_process:
-	;If ebx == 1, it's over
-	cmp ebx, 1
-	je p_return
-
-	add eax, eax
-	sub ebx, 1
-	jmp p_process
-
-p_return:
-	ret
-
-;puissance case 0
-p_0:
+get_power_2:
 	mov eax, 1
+	shl eax, cl
 	ret
 
 global main
 main:
-	mov ecx, 0 ;Init counter
-	mov edx, 0 ;Register that containt user's inputs
+	mov ebx, 0
+	mov edx, 0
 
 process:
-	;Get one input (int)
 	call get_input
 
-	;#If input == -1 : Proceed result
 	cmp eax, -1
 	je result
 
-	;#Else : Save input in ebx, init eax to 2 (for puissance)
-	mov ebx, eax
+	mov ecx, eax
+	call get_power_2
 
-	;Calculate puissance
-	call get_puissance
+	or edx, eax
 
-process_add:
-	;Add result of puissance to edx
-	add edx, eax
-
-	;Add 1 to counter
-	add ecx, 1
-
-	;#If ecx == max length : Proceed result
-	cmp ecx, LENGTH
-	je result
-
-	;#Else = Return in loop
-	jmp process
+	add ebx, 1
+	cmp ebx, LENGTH
+	jne process
 
 result:
-	mov ecx, -1 ;Init counter
+	mov ebx, -1
 
 result_loop:
-	;Increment counter
-	add ecx, 1
-
-	;#If counter == max length : End of program
-	cmp ecx, LENGTH
+	add ebx, 1
+	cmp ebx, LENGTH
 	je clean
 
-	;Put in ebx value of counter
-	mov ebx, ecx
+	mov ecx, ebx
+	call get_power_2
+	mov ecx, eax
 
-	;Get puissance (2 ^ ebx)
-	call get_puissance
-
-	;Mov result in ebx
-	mov ebx, eax
-
-	;Proceed 'and'
 	and eax, edx
-
-	;#If 'and' result == puissance : Don't show the number
-	cmp eax, ebx
+	cmp eax, ecx
 	je result_loop
 
-	;#Else: Print the number on screen
-	mov eax, ecx
+print:
+	mov eax, ebx
 	call print_int
 	call print_espace
-
-	;Loop
 	jmp result_loop
 
 clean:
